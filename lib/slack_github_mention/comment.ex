182f16cd052c208569fb4305ac761bb782e5aa4d
Bogdan Zaharia <bogdan.zaharia@kalon.ro>
2016-03-04 15:42:30 +0200

    add Comment

---
+++ lib/slack_github_mention/comment.ex
@@ -0,0 +1,19 @@
+defmodule SlackGithubMention.Comment do
+  alias SlackGithubMention.Comment
+
+  defstruct body: "", html_url: "", user_avatar_url: "", commit_id: ""
+  @opaque t :: %Comment{
+    body: String.t,
+    html_url: String.t,
+    user_avatar_url: String.t,
+    commit_id: String.t
+  }
+
+  def from_github_event(%{comment: comment}) do
+    comment_map =
+      comment
+      |> Map.take([:body, :html_url, :commit_id])
+      |> Map.put_new(:user_avatar_url, comment.user.avatar_url)
+    struct(Comment, comment_map)
+  end
+end
---
+++ lib/slack_github_mention/notifier.ex
@@ -0,0 +1,8 @@
+defmodule SlackGithubMention.Notifier do
+  alias SlackGithubMention.Comment
+
+  @spec notify_mentioned_users(Comment.t) :: nil
+  def notify_mentioned_users(comment) do
+
+  end
+end
